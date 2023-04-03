2f4f3e7ad12426fb85e0915e53285d2672494765
Othman-Adam <125485925+Othman-Adam@users.noreply.github.com>
2023-04-03 14:47:46 +0000

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  $(".hamburger-menu").click(function () {
+    $(".navigation").toggleClass("change");
+  });
+
+  lightbox.option({
+    wrapAround: true,
+  });
+
+  $(window).scroll(function () {
+    let postion = $(this).scrollTop();
+    if (postion >= 350) {
+      $(".gallery").addClass("change");
+    } else {
+      $(".gallery").removeClass("change");
+    }
+  });
+
+  $(".writers-accordion").click(function (event) {
+    if (event.target.id.split("-")[0] === "button") {
+      $("#book1").attr(
+        "src",
+        "/img/writers/" + event.target.id.split("-")[1] + "-book1.jpg"
+      );
+      $("#book2").attr(
+        "src",
+        "/img/writers/" + event.target.id.split("-")[1] + "-book2.jpg"
+      );
+    }
+  });
+});
